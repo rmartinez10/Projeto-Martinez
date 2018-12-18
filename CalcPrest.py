e6cde75ec4b9095c35de4828ab7bb1addb900a7a
RICARDO BAPTISTA MARTINEZ MELLO <R112891@sulamerica.com.br>
2018-12-18 19:10:16 -0200

    19:09

--- CalcPrest.py
+++ CalcPrest.py
@@ -1,18 +1,25 @@
 print('hello world...BBBannn')
 
 Valprest = 143.73
+ValSemJ  = 0
 Qtdprest = 1
+ValJuros = 0.073
+
+from datetime import date
+Dataatu = date.today()
+Datatexto = '{}/{}/{}'.format(Dataatu.day,Dataatu.month,Dataatu.year)
 
 #-------------------------------------------------------------------------
 print('----------------------------------------------------------------')
 print('--------------------<<<    I N I C I O    >>>-------------------')
 print('----------------------------------------------------------------')
 print('vou entrar no if...')
+Valprim = Valprest
 while Qtdprest < 24:
-   ValSemJ = Valprest + ValsemJ
+   Valsemj = Valprest + ValSemJ
    Valtotal = Valprest * Qtdprest   
    print ('Parcela: ', Qtdprest, 'custa: ',round(Valprest,2),'TOTAL PAGO: ', round(Valtotal,2) )
-   Valprest = Valprest + (Valprest * 0.073)
+   Valprest = Valprest + (Valprest * ValJuros)
    Qtdprest = Qtdprest + 1
 #print.format(Qtdprest,2)
 #elif Qtdprest > 24:
@@ -20,8 +27,13 @@ else:
     print ('---------------------------------------'            )
     print ('-----<<    E X T R A T O    >>>--------'            )
     print ('---------------------------------------'            )
+    print ('data.........................: ', Datatexto         )
+    print ('Taxa de juros................: ', ValJuros          )
     print ('quantidade de parcelas.......: ', Qtdprest          )
-    print ('valor da ultima prestacao....: ', round(Valprest,2) )
-    print ('total sem juros..............: ', round(ValSemJ, 2) )
-    print ('total com juros..............: ', round(Valtotal,2) )
     print ('---------------------------------------'            )
+    print ('valor da primeira prestacao..: ', round(Valprim,2 ) )
+    print ('valor da ultima prestacao....: ', round(Valprest,2) )
+    print ('---------------------------------------'            )
+    print ('total sem juros..............: ', round(Valsemj, 2) )
+    print ('total com juros..............: ', round(Valtotal,2) )
+    print ('---------------------------------------'            ) 
