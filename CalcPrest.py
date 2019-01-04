60b5946fc2d7c1831561636fa08b69da65dc3bc4
RICARDO BAPTISTA MARTINEZ MELLO <R112891@sulamerica.com.br>
2019-01-04 17:56:43 -0200

    teste

--- CalcPrest.py
+++ CalcPrest.py
@@ -7,7 +7,7 @@ ValJuros = 0.0073
 
 from datetime import date
 Dataatu = date.today()
-Datatexto = '{}/{}/{}'.format(Dataatu.day,Dataatu.month,Dataatu.year)
+Datatexto = '{}.{}.{}'.format(Dataatu.day,Dataatu.month,Dataatu.year)
 
 #-------------------------------------------------------------------------
 print('----------------------------------------------------------------')
@@ -18,24 +18,24 @@ Valprim = Valprest
 while Qtdprest < 24:
    ValSemJ = ValSemJ + Valprest
    Valtotal = Valprest * Qtdprest   
-   print ('Parcela: ', Qtdprest, 'custa: ',round(Valprest,2),'TOTAL PAGO: ', round(Valtotal,2) )
+   print ('==>> Parcela: ', Qtdprest, 'custa: ',round(Valprest,2),'TOTAL PAGO: ', round(Valtotal,2) )
    Valprest = Valprest + (Valprest * ValJuros)
    Qtdprest = Qtdprest + 1
 #print.format(Qtdprest,2)
 #elif Qtdprest > 24:
 else:
     Totjuros = Valtotal - ValSemJ
-    print ('---------------------------------------'            )
-    print ('-----<<    E X T R A T O    >>>--------'            )
-    print ('---------------------------------------'            )
-    print ('data.........................: ', Datatexto         )
-    print ('Taxa de juros................: ', ValJuros          )
-    print ('quantidade de parcelas.......: ', Qtdprest          )
-    print ('---------------------------------------'            )
-    print ('valor da primeira prestacao..: ', round(Valprim,2 ) )
-    print ('valor da ultima prestacao....: ', round(Valprest,2) )
-    print ('---------------------------------------'            )
-    print ('total sem juros..............: ', round(ValSemJ, 2) )
-    print ('total com juros..............: ', round(Valtotal,2) )
-    print ('total de juros pago..........: ', round(Valtotal,2) )
-    print ('---------------------------------------'            ) 
+    print ('----------------------------------------------------------------')
+    print ('-------------------<<    E X T R A T O    >>>-------------------')
+    print ('----------------------------------------------------------------')
+    print ('data.........................: ', Datatexto                      )
+    print ('Taxa de juros................: ', ValJuros                       )
+    print ('quantidade de parcelas.......: ', Qtdprest                       )
+    print ('----------------------------------------------------------------')
+    print ('valor da primeira prestacao..: ', round(Valprim,2 )              )
+    print ('valor da ultima prestacao....: ', round(Valprest,2)              )
+    print ('----------------------------------------------------------------') 
+    print ('total sem juros..............: ', round(ValSemJ, 2)              )
+    print ('total com juros..............: ', round(Valtotal,2)              )
+    print ('total de juros pago..........: ', round(Valtotal,2)              )
+    print ('----------------------------------------------------------------') 
